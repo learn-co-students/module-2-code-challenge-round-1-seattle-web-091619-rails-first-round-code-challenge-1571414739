143c57f04736d2ef16b40f4fb23f9dc2fcef26e5
Packmanager9 <ColeJDitzler@gmail.com>
2019-10-18 10:42:23 -0700

    complete
    as well as I can do from here

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,26 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+        @heroine = Heroine.find(params[:id])
+        @power = Power.find(@heroine.power_id)
+  end
+
+  def new
+    @heroine = Heroine.new
+    @heroine.name = params[:name]
+    @heroine.super_name = params[:super_name]
+    @heroine.power_id = params[:power_id]
+    @heroine.save
+    if @heroine.save
+        redirect :index
+    else
+        render :new
+    end
+
+
+  end
+  
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,6 @@
 class Heroine < ApplicationRecord
+
+    validates :name, :super_name, :power_id,  presence: true
+    validates :name, :super_name,  uniqueness: true
+    belongs_to :power
 end
